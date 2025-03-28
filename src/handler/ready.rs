b917d892f859693b9349c96024589ad4679c5cf1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-03-28 19:43:28 +0000

    feat: Update ready handler to close old LFG posts and delete global commands

--- src/handler/ready.rs
+++ src/handler/ready.rs
@@ -1,22 +1,24 @@
-use futures::future;
+use lfg::close_old_posts;
+// use futures::future;
 use serenity::all::{Context, OnlineStatus, Ready};
+use sqlx::{PgPool, Postgres};
 
 use crate::Result;
 use crate::handler::Handler;
-use crate::modules;
+use crate::modules::destiny2::lfg::LfgPostTable;
+// use crate::modules;
 
 impl Handler {
-    pub async fn ready(ctx: &Context, ready: Ready) -> Result<()> {
+    pub async fn ready(ctx: &Context, ready: Ready, pool: &PgPool) -> Result<()> {
         println!("{} is connected!", ready.user.name);
 
         ctx.set_presence(None, OnlineStatus::Online);
 
-        let futures = ready.guilds.iter().map(|guild| {
-            guild
-                .id
-                .set_commands(ctx, modules::global_register(ctx, &ready))
-        });
-        future::try_join_all(futures).await.unwrap();
+        for command in ctx.http.get_global_commands().await.unwrap() {
+            ctx.http.delete_global_command(command.id).await.unwrap();
+        }
+
+        close_old_posts::<Postgres, LfgPostTable>(ctx, pool).await;
 
         Ok(())
     }
