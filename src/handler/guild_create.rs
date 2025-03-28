d0e36acb8a1fc0b522e97f678dc8a72bd873c1c9
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-03-28 19:43:37 +0000

    feat: Add command registration for new guilds and specific handling for Bradster guild

--- src/handler/guild_create.rs
+++ src/handler/guild_create.rs
@@ -1,13 +1,29 @@
 use serenity::all::{Context, Guild};
 use sqlx::PgPool;
+use zayden_core::SlashCommand;
 
-use crate::Result;
+use crate::{
+    BRADSTER_GUILD, Result,
+    modules::{self, events::live::Live},
+};
 
 use super::Handler;
 
 impl Handler {
     pub async fn guild_create(ctx: &Context, guild: Guild, _pool: &PgPool) -> Result<()> {
         temp_voice::events::guild_create(ctx, &guild).await;
+
+        guild
+            .set_commands(ctx, modules::global_register(ctx))
+            .await
+            .unwrap();
+
+        if guild.id == BRADSTER_GUILD {
+            guild
+                .create_command(ctx, Live::register(ctx).unwrap())
+                .await
+                .unwrap();
+        }
 
         Ok(())
     }
