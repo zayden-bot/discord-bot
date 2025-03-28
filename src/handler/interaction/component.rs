840e3abb6cc27004c6d38de1b92173bdfacbb791
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-03-28 19:43:11 +0000

    feat: Add LFG interaction handling for join, leave, edit, and delete actions

--- src/handler/interaction/component.rs
+++ src/handler/interaction/component.rs
@@ -1,11 +1,14 @@
-use serenity::all::{ComponentInteraction, Context, EditInteractionResponse};
-use sqlx::PgPool;
+use serenity::all::{
+    ComponentInteraction, Context, CreateInteractionResponse, EditInteractionResponse,
+};
+use sqlx::{PgPool, Postgres};
 use suggestions::Suggestions;
 use zayden_core::ErrorResponse;
 
-use crate::Result;
 use crate::handler::Handler;
+use crate::modules::destiny2::lfg::LfgPostTable;
 use crate::modules::ticket::Ticket;
+use crate::{Error, Result};
 
 impl Handler {
     pub async fn interaction_component(
@@ -19,6 +22,62 @@ impl Handler {
         );
 
         let result = match interaction.data.custom_id.as_str() {
+            // region: Lfg
+            "lfg_join" => {
+                lfg::PostComponents::join::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_leave" => {
+                lfg::PostComponents::leave::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_alternative" => {
+                lfg::PostComponents::alternative::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_settings" => {
+                lfg::PostComponents::settings::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+
+            "lfg_edit" => {
+                lfg::SettingsComponents::edit::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_copy" => {
+                lfg::SettingsComponents::copy::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_kick" => {
+                lfg::SettingsComponents::kick::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_kick_menu" => {
+                lfg::KickComponent::run::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await
+                    .map_err(Error::from)
+            }
+            "lfg_delete" => {
+                lfg::SettingsComponents::delete::<Postgres, LfgPostTable>(ctx, interaction, pool)
+                    .await?;
+
+                interaction
+                    .create_response(ctx, CreateInteractionResponse::Acknowledge)
+                    .await
+                    .unwrap();
+
+                Ok(())
+            }
+            // "lfg_tags_add" => Lfg::tags_add(ctx, interaction).await,
+            // "lfg_tags_remove" => Lfg::tags_remove(ctx, interaction).await,
+            // endregion
             "suggestions_accept" | "suggestions_added" | "accept" => {
                 Suggestions::components(ctx, interaction, true).await;
                 Ok(())
